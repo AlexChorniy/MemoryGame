645fa50b250c1fbea8b8f22b710eb7918c35a093
oleksiichornii <alexprudent777@gmail.com>
2023-01-16 11:08:41 +0200

    Add description to getRandomData

--- src/utils/gameHelpers.ts
+++ src/utils/gameHelpers.ts
@@ -13,10 +13,12 @@ export const workWithGame: WorkWithGameType = {
     let uniqueRandomIndexes = new Set();
     const dataLength = data.length - 1;
 
+    // getting the first part of unique array of number which would be displayed
     while ([...uniqueRandomIndexes].length < data.length / 2) {
       uniqueRandomIndexes.add(generateRandomInteger(dataLength));
     }
 
+    // getting the second part of array with random duplicating
     [...uniqueRandomIndexes].map(uniqueItem => {
       let firstUniqueIndex = 0;
       let secondUniqueIndex = 0;
